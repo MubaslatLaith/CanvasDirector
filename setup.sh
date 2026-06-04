377d0fe5f39762463e7422ca87be6b561f7be547
MubaslatLaith <laithmbt@gmail.com>
2026-06-04 15:00:59 +0000

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,8 @@
+pip3 install virtualenv
+#git setup
+git config --global user.name "MubaslatLaith"
+git config --global user.email "laithmbt@gmail.com"
+git config --global credential.helper store
+git init
+
+

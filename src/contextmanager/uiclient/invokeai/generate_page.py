640ed6db5e17064812227f8045d9e5bbba985561
MubaslatLaith <laithmbt@gmail.com>
2026-06-07 16:59:58 +0000

    Move shared prompt update and invoke actions to CanvasPage

--- src/contextmanager/uiclient/invokeai/generate_page.py
+++ src/contextmanager/uiclient/invokeai/generate_page.py
@@ -4,21 +4,15 @@ from contextmanager.uiclient.invokeai.base_page import BasePage
 class GeneratePage(BasePage):
     PAGE_BUTTON = "Generate"
     INVOKE_BUTTON = "Invoke" 
-
     PROMPT_TEXT_BOX = "prompt"
     
     def update_prompt(self, prompt):
         prompt_box = self.page.locator(f"textarea[name='{self.PROMPT_TEXT_BOX}']")
-
         prompt_box.click()
-
         prompt_box.press("Control+A")
-
         prompt_box.press("Backspace")
-
         prompt_box.type(prompt)
 
 
     def invoke(self):
         self.page.get_by_role("button", name=self.INVOKE_BUTTON, exact=True).click()
-        #self.click_button(self.INVOKE_BUTTON) 

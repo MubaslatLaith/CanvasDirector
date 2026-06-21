4cb198b236deafba1d37a3c1146f7c095c98a708
MubaslatLaith <161631501+MubaslatLaith@users.noreply.github.com>
2026-06-21 14:17:53 -0700

    update sam3_test.py

--- test_perception_tools/sam3_test.py
+++ test_perception_tools/sam3_test.py
@@ -1,6 +1,9 @@
 import io
 import requests 
-import base64 
+
+import asyncio
+from contextmanager.apiclient.invokeai.client import InvokeAIClient
+
 
 def segment(image_url, prompt, threshold, mask_threshold, base_tools_url): 
     task = 'segment'
@@ -16,3 +19,31 @@ def segment(image_url, prompt, threshold, mask_threshold, base_tools_url):
     response.raise_for_status()
     segments = response.json()
     return segments 
+
+async def main():
+    INVOKE_URL = "http://127.0.0.1:9091"
+    USERNAME = "email@email.email"
+    PASSWORD = "weakpassword"
+
+
+    api_client = InvokeAIClient(INVOKE_URL)
+
+    print ('t2i output image') 
+    output_board_name = "image_to_edit_board" 
+    #output_board_name = "In Board"
+    output_image_name = api_client.boards.get_image_ids_by_board_name (output_board_name)
+    print('output image')
+    print(output_image_name)
+
+    output_image_url = api_client.images.get_image_url(output_image_name[i])
+    print (output_image_url) 
+
+
+    segmentation_prompt = "hand" 
+    base_tools_url = "http://127.0.0.1:8000/segment"
+    segmentation_output = segment(image_url = output_image_url, prompt = segmentation_prompt, threshold = 0.7, mask_threshold = 0.7, base_tools_url)
+
+
+
+
+
